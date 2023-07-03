6ed2edc6f93a8168e720a72dc78eb711edf08d72
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-07-03 14:17:50 +0530

    Time: 50 ms (56.80%) | Memory: 16.4 MB (33.48%) - LeetSync

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -8,17 +8,12 @@ class Solution:
         i = 0
         r = len(nums)-1
 
-        def swap(i,j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-
         while i<=r:
             if nums[i] == 0:
-                swap(l,i)
+                nums[l] , nums[i] = nums[i] , nums[l]
                 l+=1
             elif nums[i] == 2:
-                swap(i,r)
+                nums[i] , nums[r] = nums[r] , nums[i]
                 r-=1
                 i-=1
             # do not incremet if 2 is found
