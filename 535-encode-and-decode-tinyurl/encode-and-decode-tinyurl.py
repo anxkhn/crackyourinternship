98b4165b98e59137d31a5d2fed09697fed8d9586
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-06 23:38:52 +0530

    Time: 40 ms (71.31%) | Memory: 16.4 MB (12.47%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -0,0 +1,17 @@
+class Codec:
+
+    links = {}
+    links2 = {}
+
+    def encode(self, longUrl: str) -> str:
+        x = len(self.links)
+        self.links[longUrl] = x
+        self.links2[x] = longUrl
+        return x        
+
+    def decode(self, shortUrl: str) -> str:
+        return self.links2[shortUrl]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
