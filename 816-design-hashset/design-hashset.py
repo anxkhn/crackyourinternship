f44d4bd0f80357f911f00ec7ef918cd390f9aae0
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-14 16:59:11 +0530

    Time: 119 ms (98.63%) | Memory: 22.1 MB (55.79%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -1,17 +1,18 @@
 class MyHashSet:
 
     def __init__(self):
-        self.dict={}
+        self.my_set=set()
 
     def add(self, key: int) -> None:
-        self.dict[key]=True
+        self.my_set.add(key)
 
     def remove(self, key: int) -> None:
-        self.dict[key]=False
+        if key in self.my_set:
+            self.my_set.remove(key)
 
     def contains(self, key: int) -> bool:
-        if key in self.dict:
-            return self.dict[key]
+        if key in self.my_set:
+            return True
         else:
             return False
 
