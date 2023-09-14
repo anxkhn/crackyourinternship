e18b8d47f0dc448b069e2209d6fadf8fb7862247
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-14 11:05:29 +0530

    Time: 1132 ms (22.55%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,8 @@
-SELECT author_id AS id 
-FROM views 
-WHERE author_id = viewer_id 
-GROUP BY id 
-ORDER BY id ASC;
+SELECT DISTINCT
+    author_id AS id
+FROM
+    Views
+WHERE
+    author_id = viewer_id
+ORDER BY
+    id
