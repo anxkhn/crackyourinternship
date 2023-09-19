092ddaf605a5dfaf0d1723dc923435174c155df4
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-19 22:57:41 +0530

    Time: 68 ms (70.23%) | Memory: 16.1 MB (99.75%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,11 +1,21 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        window = len(s1)
-        s1_count = Counter(s1)
+    def checkInclusion(self, s1: str, s2: str) -> bool: 
         
-        for i in range(len(s2)-window+1):
-            s2_count = Counter(s2[i:i+window])
-            if s2_count == s1_count:
-                return True
+        if len(s1) > len(s2): return False
+                
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        
+        window_len = len(s1)
+        
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+    
+        for j in range(window_len, len(s2)):
+            if s1_count == s2_count: return True
             
-        return False
+            s2_count[ord(s2[j-window_len]) - ord('a')] -= 1
+            s2_count[ord(s2[j]) - ord('a')] += 1
+        return s1_count == s2_count     
+        
