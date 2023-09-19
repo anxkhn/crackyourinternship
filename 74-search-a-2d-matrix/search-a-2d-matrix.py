8dc0ef251f70ff639e801d7b82b2cbe563f2b4e1
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-19 19:31:43 +0530

    Time: 54 ms (30.08%) | Memory: 16.7 MB (98.46%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,29 +1,23 @@
 class Solution:
-    def check(index,c):
-        return index//c, index%c
-        # divmod function does the same thing.
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix:
             return False
 
-        l = 0
-        r = (len(matrix)*len(matrix[0])) - 1
+        rows = len(matrix)
+        columns = len(matrix[0])
 
-        rows =len(matrix)
-        columns= len(matrix[0])
+        l, r = 0, rows * columns - 1
 
         while l <= r:
-    
             mid = l + (r - l) // 2
+            x, y = divmod(mid, columns)
+            mid_val = matrix[x][y]
 
-            x,y = divmod(mid,columns)
-            if matrix[x][y] == target:
+            if mid_val == target:
                 return True
-    
-            elif matrix[x][y] < target:
+            elif mid_val < target:
                 l = mid + 1
-    
             else:
                 r = mid - 1
+
         return False
