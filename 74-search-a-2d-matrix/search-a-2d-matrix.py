c536ef262b85cb1daa02c8f0706a68eec3e9645b
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-19 19:30:00 +0530

    Time: 44 ms (89.48%) | Memory: 16.7 MB (85.02%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def check(index,c):
+        return index//c, index%c
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+
+        l = 0
+        r = (len(matrix)*len(matrix[0])) - 1
+
+        rows =len(matrix)
+        columns= len(matrix[0])
+
+        while l <= r:
+    
+            mid = l + (r - l) // 2
+
+            x,y = Solution.check(mid,columns)
+            if matrix[x][y] == target:
+                return True
+    
+            elif matrix[x][y] < target:
+                l = mid + 1
+    
+            else:
+                r = mid - 1
+        return False
