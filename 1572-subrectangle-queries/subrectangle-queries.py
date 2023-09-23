ca6096189eb532dd2cd589d6f65a7527617ffc89
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-23 15:42:40 +0530

    Time: 165 ms (47.09%) | Memory: 18.1 MB (94.01%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.py
+++ 1572-subrectangle-queries/subrectangle-queries.py
@@ -1,14 +1,27 @@
 class SubrectangleQueries:
 
+    # def __init__(self, rectangle: List[List[int]]):
+    #     self.rectangle = rectangle
+    #     self.ops = []
+
+    # def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+    #     self.ops.append((row1, col1, row2, col2, newValue))
+
+    # def getValue(self, row: int, col: int) -> int:
+    #     for row1, col1, row2, col2, val in reversed(self.ops):
+    #         if row >= row1 and col >= col1 and row <= row2 and col <= col2:
+    #             return val
+    #     return self.rectangle[row][col]
+
+
     def __init__(self, rectangle: List[List[int]]):
-        self.rectangle = rectangle
-        self.ops = []
+        self.rec = rectangle
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        self.ops.append((row1, col1, row2, col2, newValue))
+        for r in range(row1, row2+1): 
+            for c in range(col1, col2+1):
+                self.rec[r][c] = newValue
+        
 
     def getValue(self, row: int, col: int) -> int:
-        for row1, col1, row2, col2, val in reversed(self.ops):
-            if row >= row1 and col >= col1 and row <= row2 and col <= col2:
-                return val
-        return self.rectangle[row][col]
+        return self.rec[row][col]
