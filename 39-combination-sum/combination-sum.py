14d9583f4669c37cb3d774485024e6e09ebb0e06
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2023-09-30 20:59:37 +0530

    Time: 58 ms (71.24%) | Memory: 16.3 MB (75.02%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,16 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        ds = []
+        current_list = []
         def findCombination(ind: int, target: int):
             if ind == len(candidates):
                 if target == 0:
-                    ans.append(ds[:])
+                    ans.append(current_list[:])
                 return
+            #pick
             if candidates[ind] <= target:
-                ds.append(candidates[ind])
+                current_list.append(candidates[ind])
                 findCombination(ind, target - candidates[ind])
-                ds.pop()
+                current_list.pop()
+            #not pick
             findCombination(ind + 1, target)
+        
         findCombination(0, target)
         return ans
