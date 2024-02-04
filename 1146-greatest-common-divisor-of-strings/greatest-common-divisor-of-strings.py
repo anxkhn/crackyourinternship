5a5589e48a9d3443c40c31c66021f15e327e723c
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2024-02-04 16:18:50 +0530

    Time: 27 ms (97.23%) | Memory: 16.7 MB (61.41%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,14 +1,18 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         def common_factors(num1, num2):
-            return sorted(set(i for i in range(1, min(num1, num2) + 1) if num1 % i == num2 % i == 0), reverse=True)
+            common_factors_set = set()
+            min_num = min(num1, num2)
+            for i in range(1, min_num + 1):
+                if num1 % i == num2 % i == 0:
+                    common_factors_set.add(i)
+            return sorted(list(common_factors_set), reverse=True)
         x = len(str1)
         y = len(str2)
         lst = common_factors(x, y)
         for l in lst:
             first = x//l
             second = y//l
-            print(l,first,second)
             if str2[:l]*second == str2:
                 if str2[:l]*first == str1:
                     return str2[:l]
